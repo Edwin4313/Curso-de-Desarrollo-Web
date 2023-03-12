5005d0846ba86c63d41578c0b9b899737fbd40bf
Edev <caizadavid417@gmail.com>
2023-03-12 09:47:25 -0500

    9:47 - 12/3/2023

--- 6.- Curso-JavaScript/4-Operadores/function.js
+++ 6.- Curso-JavaScript/4-Operadores/function.js
@@ -6,7 +6,18 @@ var name = 'edev',
 // operadores matematicos
 var num1, num2 ;
 num1 = 4;
-num2 = 5;
+num2 = 10;
 
 var resultado = num1 + num2;
+// resta - 
+// multiplicación
+// divición /
+// mod % (lo que sobra de la divición)
 console.log(resultado)
+
+
+// operadores de incremento y decremento
+var num_ = 2;
+var resp = num_ - 1
+
+console.log(resp)
---
+++ 6.- Curso-JavaScript/Cosas/prueba.js
@@ -0,0 +1,11 @@
+
+console.log('Edev')
+
+var name = 'edev',
+    name2 = 'algo';
+
+if (name === 'edev') {
+    console.log('True')
+}else {
+    console.log('False')
+}
